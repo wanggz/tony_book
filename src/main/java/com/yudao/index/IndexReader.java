1a57ddd68e40421beff08bd11a434276c0c53097
guangzhi.wang <hdwgz1990>
2019-12-25 21:09:25 +0800

    索引查询

--- src/main/java/com/yudao/index/IndexReader.java
+++ src/main/java/com/yudao/index/IndexReader.java
@@ -1,9 +1,13 @@
 package com.yudao.index;
 
 import com.yudao.constant.Constant;
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.index.DirectoryReader;
 import org.apache.lucene.index.Term;
+import org.apache.lucene.queryparser.classic.MultiFieldQueryParser;
+import org.apache.lucene.queryparser.classic.ParseException;
 import org.apache.lucene.search.*;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
@@ -20,9 +24,79 @@ public class IndexReader {
 
         int pageStart = 0;//开始ID
         int pageSize = 10;//每页大小
-        String field = "title";//查找域
-        String keywords = "中国";//关键字
-        String sortField = "datetime";//排序关键字
+        int field = 0;//查找域
+        String keyword = "印度";//关键字
+
+        search(pageStart,pageSize,field,keyword);
+
+    }
+
+    private static void search(int pageStart, int pageSize, int field, String keyword){
+
+        String[] fields;
+        switch (field){
+            case 0:
+                fields = new String[]{"name", "author", "publisher"};
+            case 1:
+                fields = new String[]{"name"};
+            case 2:
+                fields = new String[]{"author"};
+            case 3:
+                fields = new String[]{"publisher"};
+            default:
+                fields = new String[]{"name", "author", "publisher"};
+        }
+
+        Analyzer analyzer = new StandardAnalyzer();
+        MultiFieldQueryParser parser=new MultiFieldQueryParser(fields,analyzer);
+
+        try {
+            Query query=parser.parse(keyword);
+
+            Directory dir = FSDirectory.open(Paths.get(Constant.INDEX_PATH));
+            org.apache.lucene.index.IndexReader reader = DirectoryReader.open(dir);
+            IndexSearcher search = new IndexSearcher(reader);
+
+            //------------查询
+            //searcher.search(query, LuceneManagerImpl.DEFAULT_QUERY_NUM);
+            int numHits = search.count(query);
+            if(numHits > 0) {
+                //按照时间倒序
+                Sort sort = new Sort();
+                sort.setSort(new SortField("id", SortField.Type.LONG, true));//倒序
+
+                //分页
+                TopFieldCollector c = TopFieldCollector.create(sort, pageStart+pageSize, false, false, false);
+                search.search(query, c);
+                ScoreDoc[] hits = c.topDocs(pageStart, pageSize).scoreDocs;
+                System.out.println("匹配[" + keyword + "],总共查询到" +  numHits + "个文档.");
+                if(hits.length > 0) {
+                    for (int i = 0; i < hits.length; i++) {
+                        int docID = hits[i].doc;
+                        Document doc = search.doc(docID);
+                        System.out.println("文档ID:" + docID + "  书ID:" + doc.get("id")
+                                + "   书名:" + doc.get("name") + "  作者:" + doc.get("author")
+                                +"  出版社:" + doc.get("publisher") + "  数量:" + doc.get("count")
+                                + "  地址:" + doc.get("address"));
+                    }
+                }
+            }
+            reader.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+
+    private static void search2(){
+        int pageStart = 0;//开始ID
+        int pageSize = 10;//每页大小
+        String field = "name";//查找域
+        String keywords = "印度";//关键字
+        String sortField = "id";//排序关键字
 
         try {
             Directory dir = FSDirectory.open(Paths.get(Constant.INDEX_PATH));
@@ -59,6 +133,6 @@ public class IndexReader {
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
+
 }
--- src/main/java/com/yudao/index/IndexWriter.java
+++ src/main/java/com/yudao/index/IndexWriter.java
@@ -29,89 +29,42 @@ public class IndexWriter {
 
     public static void main(String[] args) {
 
-        List<News> news = new ArrayList<>();
-        News news1 = new News();
-        news1.setId(1);
-        news1.setTitle("中国对大胜日本队");
-        news1.setContent("这是一条足球新闻");
-        news1.setDatetime("2017-01-12 12:12:12");
-
-        News news2 = new News();
-        news2.setId(2);
-        news2.setTitle("中国对大胜美国队");
-        news2.setContent("这是一条篮球新闻");
-        news2.setDatetime("2017-01-01 12:01:13");
-
-        News news3 = new News();
-        news3.setId(3);
-        news3.setTitle("食品安全的问题");
-        news3.setContent("这食品安全新闻");
-        news3.setDatetime("2011-02-22 09:11:12");
-
-        News news4 = new News();
-        news4.setId(4);
-        news4.setTitle("中国好声音");
-        news4.setContent("这是一条娱乐新闻");
-        news4.setDatetime("2017-01-12 10:12:12");
-
-        News news5 = new News();
-        news5.setId(6);
-        news5.setTitle("我爱你中国");
-        news5.setContent("这是一首歌");
-        news5.setDatetime("2011-01-11 01:10:12");
-
-        news.add(news1);
-        news.add(news2);
-        news.add(news3);
-        news.add(news4);
-        news.add(news5);
-        try {
-            org.apache.lucene.index.IndexWriter writer = LucenceUtils.getWriter(FSDirectory.open(Paths.get(Constant.INDEX_PATH)));
-            for(News n : news) {
-                Document doc = new Document();
-                doc.add(new StringField("content",n.getContent(), Field.Store.YES));
-                doc.add(new StringField("title",n.getTitle(), Field.Store.YES));
-                doc.add(new StringField("datetime",n.getDatetime(), Field.Store.YES));
-                //用于排序
-                doc.add(new NumericDocValuesField("datetime", DateUtils.toDate(n.getDatetime(), DateUtils.YYYY_MM_DD_HH_MM_SS).getTime()));
-                doc.add(new StringField("id" , n.getId()+"", Field.Store.YES));
-
-                Term id=new Term("id",n.getId()+"");
-
-                writer.updateDocument(id,doc);
-            }
-            LucenceUtils.closeWriter(writer);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-
+        loadData();
     }
 
-    private void loadData(){
+    private static void loadData(){
         String file = System.getProperty("user.dir") + "/files/books_en.xls";
         File file1 = new File(file);
         try {
             org.apache.lucene.index.IndexWriter writer = LucenceUtils.getWriter(FSDirectory.open(Paths.get(Constant.INDEX_PATH)));
             ImportExcelData data = readExcel2(file1);
             for (List<String> item : data.getFirstSheetList()) {
-                Book book = new Book(item.get(0),item.get(1),item.get(2),item.get(3),item.get(4),item.get(5),item.get(6));
+                String no = item.size()>0?item.get(0):"";
+                String index = item.size()>1?item.get(1):"";
+                String name = item.size()>2?item.get(2):"";
+                String author = item.size()>3?item.get(3):"";
+                String publisher = item.size()>4?item.get(4):"";
+                String count = item.size()>5?item.get(5):"";
+                String address = item.size()>6?item.get(6):"";
+
+                Book book = new Book(no, index, name, author, publisher, count, address);
 
                 Document doc = new Document();
-                //doc.add(new StringField("content",n.getContent(), Field.Store.YES));
-                //doc.add(new StringField("title",n.getTitle(), Field.Store.YES));
-                //doc.add(new StringField("datetime",n.getDatetime(), Field.Store.YES));
+                doc.add(new StringField("name",book.getName(), Field.Store.YES));
+                doc.add(new StringField("author",book.getAuthor(), Field.Store.YES));
+                doc.add(new StringField("publisher",book.getPublisher(), Field.Store.YES));
+                doc.add(new StringField("count",book.getCount(), Field.Store.YES));
+                doc.add(new StringField("address",book.getAddress(), Field.Store.YES));
                 //用于排序
-                //doc.add(new NumericDocValuesField("datetime", DateUtils.toDate(n.getDatetime(), DateUtils.YYYY_MM_DD_HH_MM_SS).getTime()));
-                //doc.add(new StringField("id" , n.getId()+"", Field.Store.YES));
+                doc.add(new StringField("index", book.getIndex(), Field.Store.YES));
+                doc.add(new StringField("id" , book.getNo(), Field.Store.YES));
 
                 Term id=new Term("id",book.getNo());
 
                 writer.updateDocument(id,doc);
             }
 
-
-
+            LucenceUtils.closeWriter(writer);
         } catch (Exception e) {
             e.printStackTrace();
         }
