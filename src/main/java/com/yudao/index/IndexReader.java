9937b6947961bb7b309d405d50e841f2aa7cdb42
guangzhi.wang <hdwgz1990>
2019-12-27 17:39:15 +0800

    ajax控件

--- src/main/java/com/yudao/index/IndexReader.java
+++ src/main/java/com/yudao/index/IndexReader.java
@@ -1,6 +1,7 @@
 package com.yudao.index;
 
 import com.yudao.constant.Constant;
+import com.yudao.entity.Book;
 import com.yudao.entity.Result;
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
@@ -15,6 +16,8 @@ import org.apache.lucene.store.FSDirectory;
 
 import java.io.IOException;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by guangzhi.wang on 2019/12/4/004.
@@ -28,13 +31,15 @@ public class IndexReader {
         String field = "全部";//查找域
         String keyword = "印度";//关键字
 
-        search(pageStart,pageSize,field,keyword);
+        Result result = search(pageStart,pageSize,field,keyword);
+        System.out.println(result.getNumHits());
+        System.out.println(result.getBooks().get(0).getName());
 
         //search2();
 
     }
 
-    public static void search(int pageStart, int pageSize, String field, String keyword){
+    public static Result search(int pageStart, int pageSize, String field, String keyword){
 
         Result result = new Result();
 
@@ -64,12 +69,10 @@ public class IndexReader {
             org.apache.lucene.index.IndexReader reader = DirectoryReader.open(dir);
             IndexSearcher search = new IndexSearcher(reader);
 
-            //------------查询
-            //searcher.search(query, LuceneManagerImpl.DEFAULT_QUERY_NUM);
             int numHits = search.count(query);
             result.setNumHits(numHits);
-            TopDocs topDocs = search.search(query,pageSize);
 
+            List<Book> books = new ArrayList<>();
             if(numHits > 0) {
                 //按照时间倒序
                 Sort sort = new Sort();
@@ -84,65 +87,20 @@ public class IndexReader {
                     for (int i = 0; i < hits.length; i++) {
                         int docID = hits[i].doc;
                         Document doc = search.doc(docID);
-                        System.out.println("文档ID:" + docID + "  书ID:" + doc.get("id")
-                                + "   书名:" + doc.get("name") + "  作者:" + doc.get("author")
-                                +"  出版社:" + doc.get("publisher") + "  数量:" + doc.get("count")
-                                + "  地址:" + doc.get("address"));
+
+                        books.add(new Book(doc.get("id"),doc.get("index"),doc.get("name"),doc.get("author"),doc.get("publisher"),doc.get("count"),doc.get("address")));
                     }
                 }
             }
             reader.close();
-
+            result.setBooks(books);
         } catch (Exception e) {
             e.printStackTrace();
+        } finally {
+            return result;
         }
-
 
     }
 
-
-    private static void search2(){
-        int pageStart = 0;//开始ID
-        int pageSize = 10;//每页大小
-        String field = "name";//查找域
-        String keywords = "印度";//关键字
-        String sortField = "id";//排序关键字
-
-        try {
-            Directory dir = FSDirectory.open(Paths.get(Constant.INDEX_PATH));
-            org.apache.lucene.index.IndexReader reader = DirectoryReader.open(dir);
-            IndexSearcher search = new IndexSearcher(reader);
-
-//            //WildcardQuery 模糊查询  类似于mysql like %中国%
-            keywords = "*" + keywords + "*";
-            Query query = new WildcardQuery(new Term(field, keywords));
-            //Query query = new TermQuery(new Term(field, keywords));
-            int numHits = search.count(query);
-
-            if(numHits > 0) {
-                //按照时间倒序
-                Sort sort = new Sort();
-                //sort.setSort(new SortField(sortField, SortField.Type.STRING, true));//倒序
-
-                //分页
-                TopFieldCollector c = TopFieldCollector.create(sort, pageStart+pageSize, false, false, false);
-                search.search(query, c);
-                ScoreDoc[] hits = c.topDocs(pageStart, pageSize).scoreDocs;
-                System.out.println("匹配[" + keywords + "],总共查询到" +  numHits + "个文档.");
-                if(hits.length > 0) {
-                    for (int i = 0; i < hits.length; i++) {
-                        int docID = hits[i].doc;
-                        Document doc = search.doc(docID);
-                        System.out.println("文档ID:" + docID + "  新闻ID:" + doc.get("id")
-                                + "   新闻标题:" + doc.get("name") + "  新闻内容:" + doc.get("publisher")
-                                +"  时间:" + doc.get("count"));
-                    }
-                }
-            }
-            reader.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
 
 }
